6a830b3832648e1de461b6c7aab8236613e0084d
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2024-03-24 22:28:05 -0500

    About Section 1

--- src/components/global/Sidebar.tsx
+++ src/components/global/Sidebar.tsx
@@ -12,11 +12,11 @@ const NavLinks = [
     title: 'Gallery',
     url: '/gallery',
   },
-  {
-    id: 3,
-    title: 'Booking',
-    url: '/booking',
-  },
+  // {
+  //   id: 3,
+  //   title: 'Booking',
+  //   url: '/booking',
+  // },
   // {
   //   id: 4,
   //   title: 'Shop',
@@ -54,10 +54,10 @@ const Alex = Satisfy({
 const Sidebar = () => {
   return (
     <div
-      className={`flex h-full flex-col bg-steeldark-900 pt-5 ${Alex.className}`}
+      className={`flex h-full flex-col bg-steeldark-900 py-5 ${Alex.className}`}
     >
       <SocialLinks />
-      <div className='mt-10 flex flex-col space-y-16 px-8'>
+      <div className='mt-10 flex flex-col space-y-16 px-16'>
         {NavLinks.map((link) => (
           <Link
             href={link.url}
