04d4e75a85ebf6140f03ffd08742bfd0dfa8ed5d
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2024-04-27 16:19:38 -0500

    .

--- src/app/(user)/not-found.tsx
+++ src/app/(user)/not-found.tsx
@@ -1,4 +1,4 @@
-export default function notFound() {
+export default function NotFound() {
     return (
         <div className="mx-auto space-y-3 text-center">
             <h1 className="text-4xl font-bold">404 Page Not Found</h1>
